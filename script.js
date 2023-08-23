197d945da4429c8ce3623ecc97275c5c24eda8f2
DeepThePatel <deepkalpana1@aol.com>
2023-08-22 20:58:41 -0400

    website wip

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const pdfContainer = document.getElementById("pdfContainer");
   
     pdfContainer.addEventListener("click", function () {
-
       const pdfUrl = "Resume.pdf";
-      
+      //Open PDF in new window
       window.open(pdfUrl, "_blank");
     });
   });
