5461400421fe6bd8e89226e5029b97d340df1469
DeepThePatel <deepkalpana1@aol.com>
2023-08-26 15:38:04 -0400

    tweaks

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //RESUME
 document.addEventListener("DOMContentLoaded", function () {
-  const RESUME_LINK = document.getElementById("RESUME_LINK");
+  const pdfContainer = document.getElementById("RESUME_LINK");
 
-  RESUME_LINK.addEventListener("click", function () {
+  pdfContainer.addEventListener("click", function () {
     const resumeUrl = "Resume.pdf";
     //Open PDF in new window
     window.open(resumeUrl, "_blank");
