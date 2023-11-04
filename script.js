2875049f30a20cf03e144839d546b5fcf8b14aa4
Deep Patel <deepkalpana1@aol.com>
2023-11-03 22:43:26 -0400

    added gallery modals

--- script.js
+++ script.js
@@ -8,5 +8,28 @@ document.addEventListener("DOMContentLoaded", function () {
     window.open(resumeUrl, "_blank");
   });
 });
-
+ 
 const pdfContainer = document.querySelector(".resume-section");
+
+//MODAL
+const gallery = document.querySelector(".gallery");
+const modal = document.getElementById("myModal");
+const modalImage = document.getElementById("modalImage");
+const closeBtn = document.querySelector(".close");
+
+gallery.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        modal.style.display = "block";
+        modalImage.src = e.target.src;
+    }
+});
+
+closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+modal.addEventListener("click", function (e) {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
