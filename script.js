bba15e502006a989fee5b46ca533a60f2a0ede22
Deep Patel <deepkalpana1@aol.com>
2024-01-12 19:15:47 -0500

    updated resume

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const pdfContainer = document.getElementById("RESUME_LINK");
 
   pdfContainer.addEventListener("click", function () {
-    const resumeUrl = "Resume.pdf";
+    const resumeUrl = "Resume_DeepPatel.pdf";
     //Open PDF in new window
     window.open(resumeUrl, "_blank");
   });
